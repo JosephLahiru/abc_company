1c854f7a9b0088ef2be11b4ebb4be49112d3bc44
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-21 15:39:58 +0530

    improved UI/UX in add_back_account form

--- application/views/add_bank_account_view.php
+++ application/views/add_bank_account_view.php
@@ -4,19 +4,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add Bank Account</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+
+        h2 {
+            color: #333;
+        }
+
+        form {
+            width: 400px;
+            margin: 0 auto;
+        }
+
+        label {
+            display: block;
+            margin-top: 20px;
+        }
+
+        input[type="text"],
+        input[type="submit"] {
+            width: 90%;
+            padding: 10px;
+
+        }
+
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: white;
+            cursor: pointer;
+            margin-top: 50px;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
 
 <body>
     <h2>Add Bank Account</h2>
     <?php echo validation_errors(); ?>
     <?php echo form_open('BankAccountController/add'); ?>
-    <label for="bank_name">Bank Name:</label>
-    <input type="text" id="bank_name" name="bank_name"><br>
-    <label for="branch">Branch:</label>
-    <input type="text" id="branch" name="branch"><br>
-    <label for="account_number">Account Number:</label>
-    <input type="text" id="account_number" name="account_number"><br>
+    <label for="bank_name">Bank Name</label>
+    <input type="text" id="bank_name" name="bank_name">
+    <label for="branch">Branch</label>
+    <input type="text" id="branch" name="branch">
+    <label for="account_number">Account Number</label>
+    <input type="text" id="account_number" name="account_number">
     <input type="submit" name="submit" value="Add Bank Account">
     </form>
 </body>

eedf8afe78d8cacf4695b986171f2fa2b4d61648
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-28 00:22:13 +0530

    Update BankAccountController.php

--- application/controllers/BankAccountController.php
+++ application/controllers/BankAccountController.php
@@ -15,23 +15,40 @@ class BankAccountController extends CI_Controller
 
     public function add()
     {
-        // validate input data
+        // Load the form validation library
         $this->load->library('form_validation');
+
+        // Set validation rules
         $this->form_validation->set_rules('bank_name', 'Bank Name', 'required');
         $this->form_validation->set_rules('branch', 'Branch', 'required');
-        $this->form_validation->set_rules('account_number', 'Account Number', 'required');
+
+        // Validate bank account number: allow only numbers
+        $this->form_validation->set_rules('account_number', 'Account Number', 'required|numeric');
 
         if ($this->form_validation->run() == FALSE) {
+            // If validation fails, load the add_bank_account_view to show validation errors
             $this->load->view('add_bank_account_view');
         } else {
+            // If validation succeeds, insert the data into the database
             $data = array(
                 'bank_name' => $this->input->post('bank_name'),
                 'branch' => $this->input->post('branch'),
                 'account_number' => $this->input->post('account_number')
             );
+
+            // Sanitize input to prevent XSS and injection attacks
+            $data = $this->security->xss_clean($data);
+
             $this->BankAccountModel->insert_account($data);
+
+            // Add a JavaScript alert to inform the user
+            echo '<script>alert("Bank account added successfully.");</script>';
+
+            // Redirect to the index page after a short delay (e.g., 2 seconds)
+            echo '<script>setTimeout(function(){window.location.href="' . site_url('BankAccountController/index') . '";}, 1000);</script>';
         }
     }
+
     public function delete($id)
     {
         $this->BankAccountModel->delete_account($id);
